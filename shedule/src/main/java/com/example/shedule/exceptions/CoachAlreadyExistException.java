d4868fc86efd87b4f781d752844ab7502ee6841c
Nikita60012 <96587581+Nikita60012@users.noreply.github.com>
2023-02-02 01:34:15 +0300

    Add files via upload

---
+++ shedule/src/main/java/com/example/shedule/exceptions/CoachAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.example.shedule.exceptions;
+
+public class CoachAlreadyExistException extends Exception{
+    public CoachAlreadyExistException(String message) {
+        super(message);
+    }
+}
---
+++ shedule/src/main/java/com/example/shedule/exceptions/CoachNotFoundException.java
@@ -0,0 +1,5 @@
+package com.example.shedule.exceptions;
+
+public class CoachNotFoundException extends Exception{
+    public CoachNotFoundException(String message) { super(message);}
+}
